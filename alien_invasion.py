a6912922368ca4877251343b469849e48152ab8c
Harkirats043 <Harkirats043@gmail.com>
2023-10-10 00:59:49 -0400

    adding new file

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,6 @@
 import sys
 from settings import Settings
+from ship import Ship
 import pygame
 
 class AlienInvasion:
@@ -12,21 +13,35 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode(self.settings.screen_width,
                                               self.settings.screen_height)
         pygame.display.set_caption("Alien Invasion")
+        self.ship=Ship(self)
         
         #Set the background color
         self.bg_color=(230,230,230)
       
     def run_game(self):
         while True:
+            self.check_events()
+            self._update_screen()
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RIGHT:
+                        ## Move the ship to right
+                        self.ship.rect.x += 1
             
             # Redraw the screen during each pass through the loop
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
+            
+            def _check_events(self):
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT
+                    sys.exit()
                     
-            pygame.display.flip()
-            self.clock.tick(60)
+            def _update_screen(self):
+               pygame.display.flip()
+               self.clock.tick(60)
             
 if __name__ == "__main__":
     ai = AlienInvasion()
